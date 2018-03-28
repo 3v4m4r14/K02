454653af404674959c64ce864fda667667065a80
Eva Maria Veitmaa <evveit@ttu.ee>
2018-03-28 19:36:34 +0300

    Centered start screen

--- js/animation.js
+++ js/animation.js
@@ -4,3 +4,24 @@ $('#startBtn').click(function () {
         .to(500, 200)
         .end();
 });
+
+
+function myMove() {
+    move('#square')
+        .y(100)
+        .then()
+        .x(100)
+        .then()
+        .y(100)
+        .then()
+        .scale(1.2)
+        .then()
+        .scale(1 / 1.2)
+        .translate(-100, -200)
+        .duration('0s')
+        .pop().duration('0.2s')
+        .pop().duration('0.2s')
+        .pop().duration('0.2s')
+        .pop().duration('0.2s')
+        .end();
+}
--- js/game.js
+++ js/game.js
@@ -23,23 +23,3 @@ function removeLife() {
         gameOver();
     }
 }
-
-function myMove() {
-    move('#square')
-    .y(100)
-        .then()
-        .x(100)
-            .then()
-            .y(100)
-                .then()
-                .scale(1.2)
-                    .then()
-                    .scale(1 / 1.2)
-                    .translate(-100, -200)
-                    .duration('0s')
-                    .pop().duration('0.2s')
-                .pop().duration('0.2s')
-            .pop().duration('0.2s')
-        .pop().duration('0.2s')
-    .end();
-}
