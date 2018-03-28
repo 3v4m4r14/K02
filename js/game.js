a341cf3c7134cd2baf946bffa6d608d78e0f61d4
Eva Maria Veitmaa <evveit@ttu.ee>
2018-03-28 21:11:50 +0300

    Basic maths functionality

--- js/game.js
+++ js/game.js
@@ -2,8 +2,7 @@ var lives = 3;
 var colours = ["red", "blue"];
 
 function Square() {
-    var colourIdx = Math.random() * 2;
-
+    var colourIdx = Math.round(Math.random());
     this.colour = colours[colourIdx];
 }
 
@@ -23,3 +22,33 @@ function removeLife() {
         gameOver();
     }
 }
+
+function getRandomMathOperation() {
+    var a = Math.floor((Math.random() * 10) + 1);
+    var b = Math.floor((Math.random() * 10) + 1);
+    var sum = a + b;
+
+    var answerLabel = $('#answerLabel');
+    answerLabel.text(a + " + " + b + " = ");
+
+    return sum;
+}
+
+function checkAnswer(correctAnswer) {
+    var userAnswer = $('#answer').val();
+
+    console.log(correctAnswer, userAnswer);
+
+    if (correctAnswer === parseInt(userAnswer)) {
+        console.log("Correct!");
+    } else {
+        console.log("Incorrect");
+    }
+}
+
+$('#startBtn').click(function () {
+    var correctAnswer = getRandomMathOperation();
+    $('#answerCheckBtn').click(function () {
+        checkAnswer(correctAnswer);
+    });
+});
